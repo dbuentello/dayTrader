327ba6f6c3747e15b460a9f12c48bf54ced77c66
Nathan Ladd <NathanALadd@gmail.com>
2013-10-22 11:07:58 -0700

    .

---
+++ source/dayTrader/util/OrderAction_T.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package util;
+
+/**
+ * @author nathan
+ *
+ */
+public class OrderAction_T {
+    
+    public static final String SELL = "SELL";
+    public static final String BUY = "BUY";
+    public static final String SSHORT = "SSHORT";
+
+}

94a60cfaa6771d3e0a38dbba649ae19148c937b5
Nathan Ladd <NathanALadd@gmail.com>
2013-11-27 18:47:20 -0800

    Add ConfigurationManager_T class

---
+++ source/dayTrader/managers/ConfigurationManager_T.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package managers;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import util.XMLTags_T;
+import interfaces.Manager_IF;
+
+/**
+ * @author nathan
+ *
+ */
+public class ConfigurationManager_T implements Manager_IF {
+
+    private HashMap<String, String> configParams = new HashMap<String, String>();
+    
+    
+    public ConfigurationManager_T(String filePath) {
+        
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = null;
+        
+
+        File configFile = new File(filePath);
+
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
+            Document configDoc = dBuilder.parse(configFile);
+            configDoc.getDocumentElement().normalize();
+    
+            NodeList configNodes = configDoc.getDocumentElement().getChildNodes();
+            
+            for (int i = 0; i < configNodes.getLength(); i++) {
+                
+                Node node = configNodes.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element configParam = (Element) node;
+                    configParams.put(configParam.getTagName(), configParam.getTextContent());
+                }
+            }
+            
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public String getConfigParam(String paramName) {
+        return configParams.get(paramName);
+    }
+    
+    
+    /* (non-Javadoc)
+     * @see interfaces.Manager_IF#initialize()
+     */
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see interfaces.Manager_IF#sleep()
+     */
+    @Override
+    public void sleep() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see interfaces.Manager_IF#terminate()
+     */
+    @Override
+    public void terminate() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see interfaces.Manager_IF#wakeup()
+     */
+    @Override
+    public void wakeup() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
